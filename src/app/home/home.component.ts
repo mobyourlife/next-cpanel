515ed8509b3e8a68dbd676f5267de6b51b146ae2
Filipe Oliveira <contato@fmoliveira.com.br>
2016-08-17 22:25:08 -0300

    Página de listagem de sites.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
+  sites: any[];
 
   constructor() {
     // Do stuff
@@ -12,6 +13,11 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     console.log('Hello Home');
+    this.sites = [
+      { id: 1, title: 'Fanpage de Teste', description: 'Nossa fanpage de testes gerais.' },
+      { id: 2, title: 'Mais um teste', description: 'Mais uma fanpage de teste que está mockada.' },
+      { id: 3, title: 'Angular 2', description: 'Angular 2 rocks!' },
+    ]
   }
 
 }
