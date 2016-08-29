88bc3f49fa67f0733172347ec28cbb7f2e44fc87
Filipe Oliveira <contato@fmoliveira.com.br>
2016-08-29 20:06:55 -0300

    Atualizando lista de páginas em uma zone.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 
 import { ApiService } from '../shared/api.service';
 import { Page } from './page.model';
@@ -11,15 +11,22 @@ export class HomeComponent implements OnInit {
   pages: Page[];
 
   constructor(
-    private api: ApiService
+    private api: ApiService,
+    private zone: NgZone
   ) { }
 
   ngOnInit() {
     console.log('Hello Home');
-    this.api.get<any>('users/me/pages')
-      .subscribe(data => {
-        this.pages = data;
-      });
+    this.refreshPages();
   }
 
+  private refreshPages() {
+    this.zone.run(() => {
+      this.api.get<any>('users/me/pages')
+        .subscribe(data => {
+          this.pages = data;
+          console.log(this.pages);
+        });
+    });
+  }
 }
