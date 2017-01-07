411520d0dd22c844a49e096017750904e87a0164
Filipe Oliveira <contato@fmoliveira.com.br>
2017-01-07 17:37:03 -0200

    Descrição da página de criação de novo site.

--- src/MeusSites/Novo.jsx
+++ src/MeusSites/Novo.jsx
@@ -26,6 +26,7 @@ export class MeusSitesNovo extends React.Component {
         <div className='jumbotron'>
           <div className='container'>
             <h1>Criar novo site</h1>
+            <p>Clique abaixo na página desejada. Em instantes o seu site estará pronto.</p>
           </div>
         </div>
         <div className='container'>
